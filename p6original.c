37586b942c6db28c7b1df0715cf6b029182bae94
B VIKRANTH REDDY <96039576+Vikranth923@users.noreply.github.com>
2022-02-09 15:32:29 +0000

    p6 to be modified

---
+++ p6original.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void input_string(char *a)
+{
+  printf("enter the string you want to reverse");
+  scanf("%s",a);
+}
+char *str_reverse(char *a)
+{
+  char *last = a;
+
+    while ( *last ) ++last;
+
+    if ( a != last )
+    {
+        for ( char *first = a; first < --last; ++first )
+        {
+            char c = *first;
+            *first = *last;
+            *last = c;
+        }
+    }
+
+    return a;
+}
+void output(char *a,char *reversea)
+{
+  printf("the rverse of %s is %s",a,reversea);
+}
+int main()
+{
+  char b[1000] = { '\0' };
+  
+  input_string(b);
+ // char reverse[1000] = str_reverse(b);
+  output(b, str_reverse(b) );
+  //output(b,reverse);
+  return 0;
+}
